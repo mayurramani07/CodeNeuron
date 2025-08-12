1e1b547ca220b477c6eaed29d93b59c6dafa56bb
Mayur Ramani <ramanimayur836@gmail.com>
2025-08-13 00:35:30 +0530

    Added Primsjs for syntax Highlighting

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import "prismjs/themes/prism-tomorrow.css"
+import prism from "prismjs"
+import { useState, useEffect } from 'react'
 import './App.css';
 
 
 const App = () => {
+
+  const [count, setCount ] = useState(0)
+
+  useEffect(() => {
+    prism.highlightAll() 
+  }, [])
   return (
     <> 
     <main>
       <div className='left'>
-        <div className='code'></div>
+        <div className='code'>
+          <pre>
+            <code className='language-javascript'>
+              {`function sum() {
+return 1 + 1;
+}`}
+            </code>
+          </pre>
+        </div>
         <div className='review'>Review</div>
       </div>
       <div className='right'></div>
