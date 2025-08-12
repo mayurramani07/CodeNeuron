64f6d268cda91727ff887af3255c31cbb160bf60
Mayur Ramani <ramanimayur836@gmail.com>
2025-08-13 01:18:14 +0530

    Enhance Layout

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import "prismjs/themes/prism-tomorrow.css"
+import Editor from "react-simple-code-editor"
 import prism from "prismjs"
 import { useState, useEffect } from 'react'
 import './App.css';
 
 
 const App = () => {
-
   const [count, setCount ] = useState(0)
+  const [code , setCode] = useState(`function sum() {
+return 1 + 1;
+}`)
 
   useEffect(() => {
     prism.highlightAll() 
@@ -17,13 +20,18 @@ const App = () => {
     <main>
       <div className='left'>
         <div className='code'>
-          <pre>
-            <code className='language-javascript'>
-              {`function sum() {
-return 1 + 1;
-}`}
-            </code>
-          </pre>
+          <Editor
+            value={code}
+            onValueChange={(code) => setCode(code)}
+            highlight={(code) => prism.highlight(code, prism.languages.javascript, "javascript")}
+            padding={10}
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: 16,
+              height: "100%",
+              width: "100%",
+            }}
+          />
         </div>
         <div className='review'>Review</div>
       </div>
